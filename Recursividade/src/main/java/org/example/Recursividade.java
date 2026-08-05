def99950c3c1088cb8774ebef2fe142620afab09
isabelMelo4 <isabelmelo630@gmail.com>
2026-08-05 19:25:00 -0300

    Aula 01 - conteúdo sobre recursividade

---
+++ Recursividade/src/main/java/org/example/Recursividade.java
@@ -0,0 +1,44 @@
+package org.example;
+
+import java.util.ArrayList;
+
+public class Recursividade {
+
+    public static void main(String[] args) {
+        ArrayList<Integer> listaSoma = new ArrayList<>();
+
+        listaSoma.add(2);
+        listaSoma.add(5);
+        listaSoma.add(8);
+        listaSoma.add(3);
+
+
+        System.out.println(somar(listaSoma));
+    }
+
+
+static int somar(ArrayList<Integer> listaSoma){
+
+
+        if (listaSoma.size() == 1){
+            return listaSoma.get(0);
+        }
+
+
+        else {
+            int result = listaSoma.get(0);
+           listaSoma.remove(0);
+            return result + somar(listaSoma);
+
+        }
+
+
+
+        }
+
+
+
+    }
+
+
+
